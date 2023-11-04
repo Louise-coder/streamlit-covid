d4765a62b140a4af5f9e503ca67f93de6b8ddf50
Louise <louloulam1606@gmail.com>
2023-11-04 15:16:18 +0100

    Many code cleaning : Renamming the main.py file into demo.py + extracting to the MAXIMUM into subfunctions + renamming functions and variables + writing all the docstrings + solving the minimizing of the extander with button toggle issue + extracting common variables into common.py script....

---
+++ src/common.py
@@ -0,0 +1,12 @@
+"""File containing all the common variables."""
+
+PROJECT_TITLE = """COVID-19 Time Series Tracker 😷"""
+CSV_FILE_PATH = "owid-covid-data.csv"
+country_options = [
+    "France",
+    "Germany",
+    "Spain",
+    "Italy",
+    "Sweden",
+    "England",
+]

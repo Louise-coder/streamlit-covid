ef85b1d3f5ac8e78ffe20793f0b36a8542f35dfe
Louise <louloulam1606@gmail.com>
2023-11-06 10:08:29 +0100

    Adding 2 dtf to the common.py file.

--- src/common.py
+++ src/common.py
@@ -1,7 +1,10 @@
 """File containing all the common variables."""
+import pandas as pd
 
 PROJECT_TITLE = """COVID-19 Time Series Tracker 😷"""
 CSV_FILE_PATH = "owid-covid-data.csv"
+
+# ? CSTES mais au travail on m'a dit que objets pas en maj... on fait quoi?
 country_options = [
     "France",
     "Germany",
@@ -9,4 +12,8 @@ country_options = [
     "Italy",
     "Sweden",
     "England",
+]
+df_covid_data = pd.read_csv("data/" + CSV_FILE_PATH)
+df_covid_data_our_countries = df_covid_data[
+    df_covid_data["location"].isin(country_options)
 ]
